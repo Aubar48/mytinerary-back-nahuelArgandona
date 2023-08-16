9779a9bbf6b7e945c222d3b73ac17e97bc425e65
“Nahuel <nnahuell.gb@hotmail.com>
2023-08-16 05:58:33 -0300

    feat:all

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
 const router = express.Router();
+import { MyTinerary } from '../MyTinerary.js'
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  console.log(req);
+  res.json({
+    data:MyTinerary,
+    success:true
+  })
 });
 
 export default router;
